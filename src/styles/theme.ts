40362bb707d766f39c72b92446127fe62fc0081c
Kan Adachi <kadachi1023@gmail.com>
2019-02-16 15:30:16 -0800

    Adds themes

---
+++ src/styles/theme.ts
@@ -0,0 +1,54 @@
+export enum ThemeName {
+  light = "LIGHT_THEME",
+  dark = "DARK_THEME",
+}
+
+export interface Theme {
+  surface: string,
+  headline: string,
+  body: string,
+  label: string,
+  hint: string,
+  hairline: string,
+  primary: string,
+  secondary: string,
+  tertiary: string,
+  error: string,
+}
+
+const lightTheme: Theme = {
+  surface: '#fff',
+  headline: '#000',
+  body: '#212121',
+  label: '#333',
+  hint: '#555',
+  hairline: '#d0d0d0',
+  primary: '#065998',
+  secondary: '#069872',
+  tertiary: '#ce6e04',
+  error: '#c71422',
+};
+
+const darkTheme: Theme = {
+  surface: '#212121',
+  headline: '#fff',
+  body: '#eaeaea',
+  label: '#cecece',
+  hint: '#adadad',
+  hairline: '#4e4e4e',
+  primary: '#53a0da',
+  secondary: '#5cdab9',
+  tertiary: '#efb16c',
+  error: '#f1505c',
+};
+
+const themes = new Map<ThemeName, Theme>([
+  [ThemeName.light, lightTheme],
+  [ThemeName.dark, darkTheme],
+]);
+
+const getTheme = (themeName: ThemeName): Theme => {
+  return themes.get(themeName) as Theme;
+};
+
+export default getTheme;
