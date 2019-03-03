fb84487ba8488b1a2581a81dd3f7ab07b0456c87
Kan Adachi <kadachi1023@gmail.com>
2019-03-03 06:07:53 -0800

    Modifies project structure to split out conflicting dependencies

---
+++ api/src/config.ts
@@ -0,0 +1,5 @@
+export const DB_HOST = 'localhost';
+export const DB_USER = 'root';
+export const DB_PASSWORD = '';
+export const DB_NAME = 'fifteen';
+export const AUTH0_API_IDENTIFIER = 'https://fifteen.io/api';
---
+++ web/src/config.ts
@@ -0,0 +1,5 @@
+export const AUTH0_DOMAIN = 'obber.auth0.com';
+export const AUTH0_CLIENT_ID = 'REur5wyNkihyed70nx6D7g5Njlb5cfzx';
+export const AUTH0_REDIRECT_URI = 'http://localhost:3000/auth/callback';
+export const AUTH0_RESPONSE_TYPE = 'token id_token';
+export const AUTH0_SCOPE = 'openid';
